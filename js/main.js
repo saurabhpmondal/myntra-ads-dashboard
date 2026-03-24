24d4ee54a44c6cbe6756bf09e6eaa5cc2445d83b
saurabhpmondal <saurabhpmondal@gmail.com>
2026-03-25 01:08:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,17 +3,25 @@
 import { renderKPICards } from "./ui/kpiCards.js";
 import { setupChartSection } from "./ui/chartSection.js";
 import { setupTabs } from "./ui/tabs.js";
+import { renderTable } from "./ui/table.js";
+import { setupSearch } from "./ui/search.js";
 
 function init() {
 
-    // Render KPI Cards (dummy for now)
+    // KPI Cards
     renderKPICards();
 
-    // Setup chart dropdowns
+    // Chart dropdowns
     setupChartSection();
 
-    // Setup tabs
+    // Tabs
     setupTabs();
+
+    // Default table
+    renderTable("campaign");
+
+    // Search UI
+    setupSearch();
 
 }
 
